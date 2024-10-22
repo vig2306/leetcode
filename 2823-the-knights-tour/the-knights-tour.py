2255be9c1c486265a3c2afc8a744fe999d98671c
Vignesh <vignesh.sunder@gmail.com>
2024-10-22 03:01:40 -0700

    Time: 2785 ms (37.50%) | Memory: 16.8 MB (8.82%) - LeetSync

--- 2823-the-knights-tour/the-knights-tour.py
+++ 2823-the-knights-tour/the-knights-tour.py
@@ -7,29 +7,30 @@ class Solution:
     def dfs(self,r,c,m,n,count):
         self.visited[r][c] = count
 
-        if count == m * n:
+        if count == (m * n)-1:
             if not self.set_final:
                 self.set_final = True
-                for i in range(len(self.final)):
-                    for j in range(len(self.final[0])):
-                        self.final[i][j] = self.visited[i][j] - 1
+                print(self.visited)
+                # for i in range(len(self.final)):
+                #     for j in range(len(self.final[0])):
+                #         self.final[i][j] = self.visited[i][j] - 1
             return
 
         for di, dj in self.moves:
             ni, nj = r + di, c + dj
 
-            if not self.is_invalid(ni, nj, m, n) and self.visited[ni][nj] == 0 and not self.set_final:
+            if not self.is_invalid(ni, nj, m, n) and self.visited[ni][nj] == -1 and not self.set_final:
                 self.dfs(ni, nj, m, n, count + 1)
-                self.visited[ni][nj] = 0
+                if not self.set_final: self.visited[ni][nj] = -1
 
 
     def tourOfKnight(self, m: int, n: int, r: int, c: int) -> List[List[int]]:
-        self.visited = [[0 for i in range(n)] for j in range(m)]
-        self.final = [[0 for i in range(n)] for j in range(m)]
+        self.visited = [[-1 for i in range(n)] for j in range(m)]
+        # self.final = [[0 for i in range(n)] for j in range(m)]
         self.set_final = False
         self.visited[r][c] = 1
         self.moves = [[1,2], [-1,2], [1,-2], [-1,-2], [2,1], [-2,1], [2,-1], [-2,-1]]
-        self.dfs(r,c,m,n,1)
+        self.dfs(r,c,m,n,0)
 
-        return self.final
+        return self.visited
         
