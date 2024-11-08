80e2c22ba6baecc4d43be9660b628ee2bd971c2e
Vignesh <vignesh.sunder@gmail.com>
2024-11-07 18:24:30 -0800

    Time: 3 ms (66.33%) | Memory: 16.8 MB (71.92%) - LeetSync

--- 1149-intersection-of-three-sorted-arrays/intersection-of-three-sorted-arrays.py
+++ 1149-intersection-of-three-sorted-arrays/intersection-of-three-sorted-arrays.py
@@ -1,19 +1,39 @@
 class Solution:
     def arraysIntersection(self, arr1: List[int], arr2: List[int], arr3: List[int]) -> List[int]:
-        freq = {}
-        for i in range(len(arr1)):
-            freq[arr1[i]] = freq.get(arr1[i], 0) + 1
-
-        for i in range(len(arr2)):
-            freq[arr2[i]] = freq.get(arr2[i], 0) + 1
-
-        for i in range(len(arr3)):
-            freq[arr3[i]] = freq.get(arr3[i], 0) + 1
-        
         res = []
-        for key, value in freq.items():
-            if value == 3:
-                res.append(key)
+        p1, p2, p3 =0,0,0
+        while p1 < len(arr1) and p2 < len(arr2) and p3 < len(arr3):
+            if arr1[p1] == arr2[p2] and arr2[p2] == arr3[p3]:
+                res.append(arr1[p1])
+                p1 += 1
+                p2 += 1
+                p3 += 1
+                continue
+            if arr1[p1] < arr2[p2]:
+                p1 += 1
+            elif arr2[p2] < arr3[p3]:
+                p2 += 1
+            else:
+                p3+= 1
+            
         return res
+
+
+            
+
+
+        # freq = {}
+        # for i in range(len(arr1)):
+        #     freq[arr1[i]] = freq.get(arr1[i], 0) + 1
+
+        # for i in range(len(arr2)):
+        #     freq[arr2[i]] = freq.get(arr2[i], 0) + 1
+
+        # for i in range(len(arr3)):
+        #     freq[arr3[i]] = freq.get(arr3[i], 0) + 1
         
-        
+        # res = []
+        # for key, value in freq.items():
+        #     if value == 3:
+        #         res.append(key)
+        # return res
