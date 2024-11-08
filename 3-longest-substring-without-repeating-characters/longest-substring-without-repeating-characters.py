4cff3e0fbc21e6153378e30e872f9ffe23a99d8d
Vignesh <vignesh.sunder@gmail.com>
2024-11-07 22:48:20 -0800

    Time: 15 ms (81.11%) | Memory: 16.6 MB (64.42%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -4,19 +4,16 @@ class Solution:
         end = 0
         n = len(s)
         longest = 0
+        visited = set()
         # print(len(s))
-        while start < n:
-            end = start
-            visited = set()
-            count = 0
-            while end < n:
-                if s[end] not in visited:
-                    visited.add(s[end])
-                    end += 1
-                else:
-                    break
-            longest = max(longest, end-start)
-            start = start + 1
+        while end < n:
+            if s[end] not in visited:
+                visited.add(s[end])
+                longest = max(longest, end-start+1)
+                end += 1          
+            else:
+                visited.remove(s[start])
+                start = start + 1
         return longest
                 
 
