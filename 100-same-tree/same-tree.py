d559a4049fb6e996171764ee470adef129a2f494
Vignesh <vignesh.sunder@gmail.com>
2024-11-12 15:16:05 -0800

    Time: 0 ms (100.00%) | Memory: 16.7 MB (24.44%) - LeetSync

---
+++ 100-same-tree/same-tree.py
@@ -0,0 +1,53 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def bfs(self, p, q):
+        queue1 = [p]
+        queue2 = [q]
+        flag = 0
+        while queue1 and queue2:
+            size1 = len(queue1)
+            size2 = len(queue2)
+            if size1 != size2:
+                flag = 1
+                break
+            for i in range(size1):
+                node1 = queue1.pop(0)
+                node2 = queue2.pop(0)
+                if node1.val != node2.val:
+                    flag = 1
+                    return False
+                if node1.left and not node2.left or not node1.left and node2.left:
+                    flag = 1 
+                    return False
+                else:
+                    if node1.left and node2.left:
+                        queue1.append(node1.left)
+                        queue2.append(node2.left)
+                if node1.right and not node2.right or not node1.right and node2.right:
+                    flag = 1 
+                    return False
+                else:
+                    if node1.right and node2.right:
+                        queue1.append(node1.right)
+                        queue2.append(node2.right)
+                    
+        if flag == 1:
+            return False
+        
+        return True
+            
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if p is None and q is None:
+            return True
+        if p is None and q is not None or p is not None and q is None:
+            return False
+        val = self.bfs(p,q)
+
+        return val
+
+        
