8bdc3baea681fa46d5d0a8de8103cc7449512527
Vignesh <vignesh.sunder@gmail.com>
2024-12-11 01:42:53 -0800

    Time: 461 ms (22.22%) | Memory: 32.6 MB (15.57%) - LeetSync

--- 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.py
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.py
@@ -1,5 +1,33 @@
 class Solution:
     def maximumBeauty(self, nums: List[int], k: int) -> int:
+        #Line Sweep
+        start = []
+        end = []
+        max_len = 0
+        for i in range(len(nums)):
+            mini = nums[i] - k
+            maxi = nums[i] + k
+            start.append(mini)
+            end.append(maxi)
+        
+        start.sort()
+        end.sort()
+        count = 0
+        start_pointer = 0
+        end_pointer = 0
+        while start_pointer < len(start) and end_pointer < len(end):
+            if start[start_pointer] <= end[end_pointer]:
+                count += 1
+                start_pointer+=1
+            else:
+                count -= 1
+                end_pointer += 1
+            max_len = max(count, max_len)
+        
+        return max_len
+
+
+        #Sliding Window
         max_len = -1
         # count = 0
         nums.sort()
