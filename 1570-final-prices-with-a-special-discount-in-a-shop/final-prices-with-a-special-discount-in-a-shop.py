cddf3c52b693ebb618a1fe066aae23fe829d8e43
Vignesh <vignesh.sunder@gmail.com>
2024-12-17 23:36:36 -0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (10.22%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
@@ -1,20 +1,32 @@
 class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
         result = [-1]*len(prices)
-        # mini = [-1]*len(prices)
-        # mini[-1] = prices[-1]
-        # for i in range(len(prices)-2, -1, -1):
+        stack = []
+        for i in range(0, len(prices)):
+            curr_val = prices[i]
+
+            while stack and curr_val <= stack[-1][0]:
+                top, index = stack.pop()
+                result[index] = top - curr_val
             
-        # result[-1] = prices[-1]
-        # for i in range(len(prices)-2, -1, -1):
-        #     mini = min(prices[i], mini)
-        #     if mini <= prices[i]:
-        #         result[i] = prices[i] - mini
-        for i in range(len(prices)):
-            for j in range(i+1, len(prices)):
-                if prices[j] <= prices[i]:
-                    result[i] = prices[i] - prices[j]
-                    break
-            if result[i] == -1:
-                result[i] = prices[i]
-        return result        
+            stack.append((curr_val, i))
+        
+        while stack:
+            top, index = stack.pop()
+            result[index] = top
+        
+        return result
+
+
+
+        #Brute Force
+        #TC -> O(N^2)
+        #SC -> O(N)
+        # for i in range(len(prices)):
+        #     for j in range(i+1, len(prices)):
+        #         if prices[j] <= prices[i]:
+        #             result[i] = prices[i] - prices[j]
+        #             break
+        #     if result[i] == -1:
+        #         result[i] = prices[i]
+        # return result        
