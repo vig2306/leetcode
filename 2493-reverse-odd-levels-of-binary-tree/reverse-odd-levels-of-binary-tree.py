0a853b46d2601becb407d7330996a2c47422ea37
Vignesh <vignesh.sunder@gmail.com>
2024-12-20 16:18:37 -0800

    Time: 55 ms (15.58%) | Memory: 23 MB (6.41%) - LeetSync

--- 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.py
+++ 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.py
@@ -1,37 +1,64 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def reverseOddLevels(self, root):
-        if not root:
-            return None  # Return None if the tree is empty.
-
-        queue = [root]  # Start BFS with the root node.
-        level = 0
-
+    def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root is None:
+            return root
+        queue = deque()
+        queue.append((root, 0))
+        res = []
         while queue:
-            size = len(queue)
-            current_level_nodes = []
-
-            # Process all nodes at the current level.
-            for _ in range(size):
-                node = queue.pop(0)
-                current_level_nodes.append(node)
-
+            length = len(queue)
+            temp = []
+            for i in range(length):
+                node, level = queue.popleft()
+                temp.append(node.val)
                 if node.left:
-                    queue.append(node.left)
+                    queue.append((node.left, level + 1))
                 if node.right:
-                    queue.append(node.right)
+                    queue.append((node.right, level + 1))
+            
+            if level%2 == 0:
+                res.append(temp)
+            else:
+                res.append(temp[::-1])
 
-            # Reverse node values if the current level is odd.
-            if level % 2 == 1:
-                left, right = 0, len(current_level_nodes) - 1
-                while left < right:
-                    tmp = current_level_nodes[left].val
-                    current_level_nodes[left].val = current_level_nodes[
-                        right
-                    ].val
-                    current_level_nodes[right].val = tmp
-                    left += 1
-                    right -= 1
+        
+        q = deque()
+        q.append((root, 0))
+        while q:
+            length = len(q)
+            j = 0
+            for i in range(length):
+                node, level = q.popleft()
+                if level + 1 < len(res):
+               
+                    node.left.val = res[level+1][j]
+                    node.right.val = res[level+1][j+1]
+                    j += 2
+                if node.left:
+                    q.append((node.left, level+1))
+                
+                if node.right:
+                    q.append((node.right, level+1))
+        
+        return root
 
-            level += 1
+                    
+                    
 
-        return root  # Return the modified tree root.
+
+        
+        return root
+
+                
+
+        
+
+
+
+        
