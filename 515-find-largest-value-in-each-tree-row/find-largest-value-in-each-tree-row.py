a45675973d42524ba2bd8b4a2315bb892de100a8
Vignesh <vignesh.sunder@gmail.com>
2024-12-24 20:58:19 -0800

    Time: 5 ms (9.78%) | Memory: 19.7 MB (8.63%) - LeetSync

--- 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.py
+++ 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.py
@@ -5,29 +5,54 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def dfs(self, node, depth):
+        if not node:
+            return
+
+        if depth == len(self.result):
+            self.result.append(node.val)
+        else:
+            self.result[depth] = max(self.result[depth], node.val)
+
+        self.dfs(node.left, depth+1)
+        self.dfs(node.right, depth+1)
+
+        return 
+         
     def largestValues(self, root: Optional[TreeNode]) -> List[int]:
-        if root is None:
-            return []
+
+        self.result = []
+        self.dfs(root, 0)
+
+        return self.result
+
+
+    #BFS
+    # TC-> O(N)
+    # SC -> O(N)
+    # def largestValues(self, root: Optional[TreeNode]) -> List[int]:
+        # if root is None:
+        #     return []
         
-        queue = deque()
-        queue.append(root)
-        result = []
-        while queue:
-            n = len(queue)
-            max_val = float('-inf')
-            for _ in range(n):
-                node = queue.popleft()
-                max_val = max(max_val, node.val)
-                if node.left:
-                    queue.append(node.left)
+        # queue = deque()
+        # queue.append(root)
+        # result = []
+        # while queue:
+        #     n = len(queue)
+        #     max_val = float('-inf')
+        #     for _ in range(n):
+        #         node = queue.popleft()
+        #         max_val = max(max_val, node.val)
+        #         if node.left:
+        #             queue.append(node.left)
                 
-                if node.right:
-                    queue.append(node.right)
+        #         if node.right:
+        #             queue.append(node.right)
             
-            result.append(max_val)
-            max_val = float('-inf')
+        #     result.append(max_val)
+        #     max_val = float('-inf')
         
-        return result
+        # return result
 
                 
 
