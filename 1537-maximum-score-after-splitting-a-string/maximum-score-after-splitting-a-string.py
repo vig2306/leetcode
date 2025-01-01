3f55dc42a4dc5d445e75d14ef12d54f45f8fe9e7
Vignesh <vignesh.sunder@gmail.com>
2024-12-31 17:29:12 -0800

    Time: 3 ms (57.89%) | Memory: 17.9 MB (9.48%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.py
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.py
@@ -1,38 +1,58 @@
 class Solution:
     def maxScore(self, s: str) -> int:
-
-        #Sub-Optimal:
-
+        #Sub-Optimal 2: Two-Pass
+        #TC -> O(2*N) -> O(N)
+        #SC -> O(1)
         max_score = 0
-        zeroes_array = [0]*len(s)
-        ones_array = [0]*len(s)
+        zeroes = 0
+        ones = 0
         for i in range(len(s)):
-            if i == 0:
-                if s[i] == '0':
-                    zeroes_array[i] = 1
-                continue
-            if s[i] == '0':
-                zeroes_array[i] = zeroes_array[i-1] + 1
-            else:
-                zeroes_array[i] = zeroes_array[i-1]
-
-        for i in range(len(s)-1,-1,-1):
-            if i == len(s)-1:
-                if s[i] == '1':
-                    ones_array[i] = 1 
-                continue
             if s[i] == '1':
-                ones_array[i] = ones_array[i+1] + 1
-            else:
-                ones_array[i] = ones_array[i+1]
+                ones+=1
         
-        # print(ones_array)
-        # print(zeroes_array)
-        for i in range(len(zeroes_array)-1):
-            curr_score = zeroes_array[i] + ones_array[i+1]
-            max_score = max(curr_score, max_score)
+        for i in range(len(s)-1):
+            if s[i] == '0':
+                zeroes += 1
+            if s[i] == '1':
+                ones -= 1
+            
+            max_score = max(max_score, zeroes+ones)
         
         return max_score
+
+        #Sub-Optimal:
+        #TC -> O(3*N) -> O(N)
+        #SC -> O(2*N) -> O(N)
+        #Approach: Find the no of zeroes on the left at each index 
+        # and the no of ones on the right at each index
+        # max_score = 0
+        # zeroes_array = [0]*len(s)
+        # ones_array = [0]*len(s)
+        # for i in range(len(s)):
+        #     if i == 0:
+        #         if s[i] == '0':
+        #             zeroes_array[i] = 1
+        #         continue
+        #     if s[i] == '0':
+        #         zeroes_array[i] = zeroes_array[i-1] + 1
+        #     else:
+        #         zeroes_array[i] = zeroes_array[i-1]
+
+        # for i in range(len(s)-1,-1,-1):
+        #     if i == len(s)-1:
+        #         if s[i] == '1':
+        #             ones_array[i] = 1 
+        #         continue
+        #     if s[i] == '1':
+        #         ones_array[i] = ones_array[i+1] + 1
+        #     else:
+        #         ones_array[i] = ones_array[i+1]
+        
+        # for i in range(len(zeroes_array)-1):
+        #     curr_score = zeroes_array[i] + ones_array[i+1]
+        #     max_score = max(curr_score, max_score)
+        
+        # return max_score
 
 
         #Brute Force
