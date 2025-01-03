4d98f1d96b8c8415ac3ba4ad7cf388d8e76b98bc
Vignesh <vignesh.sunder@gmail.com>
2025-01-02 19:14:22 -0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (12.68%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -3,15 +3,39 @@ class Solution:
         #Why is this similar to fibonnacci types:
         #Step 3 = 3 ways
         #Step 4 = 5 ways
+        #F(N) = F(N-1) + F(N-2)
 
-    
-        stepCount = []
-        #For n = 1 and 2
-        stepCount.append(1)
-        stepCount.append(2)
+        #Optimal -> Space Optimized:
+
+        #TC -> O(N)
+        #SC -> O(1)
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        
+        first = 1
+        second = 2
+        count = 0
 
         for i in range(2, n):
-            count = stepCount[i-1] + stepCount[i-2]
-            stepCount.append(count)
+            count = first + second
+            first = second
+            second = count
         
-        return stepCount[n-1]
+        return count
+
+        #Optimal -> With Space:
+        #TC -> O(N)
+        #SC -> O(N)
+
+        # stepCount = []
+        # #For n = 1 and 2
+        # stepCount.append(1)
+        # stepCount.append(2)
+
+        # for i in range(2, n):
+        #     count = stepCount[i-1] + stepCount[i-2]
+        #     stepCount.append(count)
+        
+        # return stepCount[n-1]
