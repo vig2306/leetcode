6189f5156ec4e38ca26fd29cbe0f6ff877f17fe5
Vignesh <vignesh.sunder@gmail.com>
2025-01-02 17:09:30 -0800

    Time: 39 ms (97.22%) | Memory: 32.5 MB (42.65%) - LeetSync

--- 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.py
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.py
@@ -1,16 +1,32 @@
 class Solution:
     def waysToSplitArray(self, nums: List[int]) -> int:
-        prefixSum = [nums[0]]
+        #Optimal:
+
+        totalSum = sum(nums)
         count = 0
-        for i in range(1, len(nums)):
-            prefixSum.append(nums[i]+prefixSum[i-1])
-        n = len(prefixSum)
-        print(prefixSum)
-        for i in range(len(prefixSum)-1):
-            print(prefixSum[i], prefixSum[n-i-1])
-            if prefixSum[i] >= prefixSum[n-1] - prefixSum[i]:
-                count += 1
+        curr_sum = 0
+        for i in range(len(nums)-1):
+            curr_sum += nums[i]
+            if curr_sum >= totalSum - curr_sum:
+                count+=1
         
         return count
+
+
+        #Sub-Optimal: -> Prefix Sum
+        #TC -> O(N)
+        # #SC -> O(N)
+        # prefixSum = [nums[0]]
+        # count = 0
+        # for i in range(1, len(nums)):
+        #     prefixSum.append(nums[i]+prefixSum[i-1])
+        # n = len(prefixSum)
+        # print(prefixSum)
+        # for i in range(len(prefixSum)-1):
+        #     print(prefixSum[i], prefixSum[n-i-1])
+        #     if prefixSum[i] >= prefixSum[n-1] - prefixSum[i]:
+        #         count += 1
+        
+        # return count
 
         
