2b65653a47869afe28c2306baa7f38b602979df8
Vignesh <vignesh.sunder@gmail.com>
2025-01-03 16:39:02 -0800

    Time: 8 ms (72.62%) | Memory: 17.6 MB (28.76%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,23 +1,25 @@
 class Solution:
-    def recursion(self, index, target, nums, subset):
-        if index == len(nums):
+    def recursion(self, candidates, target, index, subset):
+        if index == -1:
             if target == 0:
                 self.result.append(subset.copy())
             return
-        #PICK Element if less than curr target value
-        if nums[index] <= target:
-            subset.append(nums[index])
-            self.recursion(index, target - nums[index], nums, subset)
-            subset.pop()
-
         
-        self.recursion(index+1, target, nums, subset)
+        if target >= candidates[index]:
+            subset.append(candidates[index])
+            self.recursion(candidates, target-candidates[index], index, subset)
+            subset.pop()
+        
+        self.recursion(candidates, target, index-1, subset)
 
         return
 
+    
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+
         self.result = []
-        self.recursion(0, target, candidates, [])
+        self.recursion(candidates, target, len(candidates)-1, [])
 
         return self.result
+
         
