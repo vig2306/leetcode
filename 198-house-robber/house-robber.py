0d78aeb444c2df9b0a1d5204860a3065364aabb6
Vignesh <vignesh.sunder@gmail.com>
2025-01-08 14:25:31 -0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (12.40%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,48 +1,45 @@
 class Solution:
-    def recursion(self, nums, index):
-        if index >= len(nums):
+
+    def memoization(self, nums, index):
+        if index < 0:
             return 0
+        if index == 0:
+            self.memo[index] = nums[index]
+            return self.memo[index]
         
         if self.memo[index] != -1:
             return self.memo[index]
         
-        take = nums[index] + self.recursion(nums, index+2)
-        not_take = self.recursion(nums, index+1)
+        take = nums[index] + self.memoization(nums, index - 2)
+        not_take = self.memoization(nums, index-1)
 
         self.memo[index] = max(take, not_take)
 
-        return self.memo[index]
+        return max(take, not_take)
+        
 
     def rob(self, nums: List[int]) -> int:
-        #DP - Optimised Space:
-        two_max = 0
-        one_max = 0
-        ans = 0
-        for index in range(len(nums)-1, -1, -1):
-            take = nums[index] + two_max
-            not_take = one_max
-            ans = max(take, not_take)
-            two_max = one_max
-            one_max = ans
+        n = len(nums)
+        self.memo = [-1]*(n)
+        result = self.memoization(nums, n-1)
+        return result
+
+    # def recursion(self, nums, index):
+    #     if index < 0:
+    #         return 0
+    #     if index == 0:
+    #         return nums[index]
         
-        return ans
+    #     take = nums[index] + self.recursion(nums, index - 2)
+    #     not_take = self.recursion(nums, index-1)
+
+    #     return max(take, not_take)
         
 
+    # def rob(self, nums: List[int]) -> int:
+    #     n = len(nums)
+    #     result = self.recursion(nums, n-1)
+    #     return result
 
 
-        #DP - O(N) Space
-        tabulate = [0]*(len(nums)+2)
-
-        for index in range(len(nums)-1,-1,-1):
-
-            take = nums[index] + tabulate[index+2]
-            not_take = tabulate[index+1]
-
-            tabulate[index] = max(take, not_take)
-        
-        return tabulate[0]
-
-        #Recursion with MEMO
-        self.memo = [-1]*(len(nums))
-        return self.recursion(nums, 0)
         
