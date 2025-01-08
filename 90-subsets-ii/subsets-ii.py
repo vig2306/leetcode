274d9192ee97658dc3a85d38f131ca3ffe5ddc3e
Vignesh <vignesh.sunder@gmail.com>
2025-01-07 17:23:02 -0800

    Time: 0 ms (100.00%) | Memory: 18.2 MB (6.04%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -1,21 +1,46 @@
 class Solution:
     def recursion(self, nums, subset, index):
         if index == len(nums):
-            sub_tup = tuple(sorted(subset))
-            self.result.add(sub_tup)
-            # self.result.append(subset.copy())
+            self.result.append(subset.copy())
             return
+        
         subset.append(nums[index])
         self.recursion(nums, subset, index+1)
         subset.pop()
+        
+        while index < len(nums)-1 and nums[index] == nums[index+1]:
+            index += 1
+        
         self.recursion(nums, subset, index+1)
 
         return
+        
+
 
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        self.result = set()
+        self.result = []
+        nums.sort()
         self.recursion(nums, [], 0)
     
-        return sorted(list(self.result))
+        return self.result
+
+    # def recursion(self, nums, subset, index):
+    #     if index == len(nums):
+    #         sub_tup = tuple(sorted(subset))
+    #         self.result.add(sub_tup)
+    #         # self.result.append(subset.copy())
+    #         return
+    #     subset.append(nums[index])
+    #     self.recursion(nums, subset, index+1)
+    #     subset.pop()
+    #     self.recursion(nums, subset, index+1)
+
+    #     return
+
+    # def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+    #     self.result = set()
+    #     self.recursion(nums, [], 0)
+    
+    #     return sorted(list(self.result))
         
         
