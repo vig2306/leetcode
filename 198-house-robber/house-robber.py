3c2c7178a64620b208f2567eb0302ebe2f1e8375
Vignesh <vignesh.sunder@gmail.com>
2025-01-08 14:31:46 -0800

    Time: 1 ms (11.90%) | Memory: 17.9 MB (12.40%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,28 +1,44 @@
 class Solution:
 
-    def memoization(self, nums, index):
-        if index < 0:
-            return 0
-        if index == 0:
-            self.memo[index] = nums[index]
-            return self.memo[index]
-        
-        if self.memo[index] != -1:
-            return self.memo[index]
-        
-        take = nums[index] + self.memoization(nums, index - 2)
-        not_take = self.memoization(nums, index-1)
-
-        self.memo[index] = max(take, not_take)
-
-        return max(take, not_take)
-        
-
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        self.memo = [-1]*(n)
-        result = self.memoization(nums, n-1)
-        return result
+        dp = [-1]*n
+        dp[0] = nums[0]
+        for index in range(1, n):
+            if index - 2 < 0:
+                take = nums[index]
+            else:
+                take = nums[index] + dp[index-2]
+
+            not_take = dp[index-1]
+
+            dp[index] = max(take, not_take)
+
+        return dp[n-1]
+
+    # def memoization(self, nums, index):
+    #     if index < 0:
+    #         return 0
+    #     if index == 0:
+    #         self.memo[index] = nums[index]
+    #         return self.memo[index]
+        
+    #     if self.memo[index] != -1:
+    #         return self.memo[index]
+        
+    #     take = nums[index] + self.memoization(nums, index - 2)
+    #     not_take = self.memoization(nums, index-1)
+
+    #     self.memo[index] = max(take, not_take)
+
+    #     return max(take, not_take)
+        
+
+    # def rob(self, nums: List[int]) -> int:
+    #     n = len(nums)
+    #     self.memo = [-1]*(n)
+    #     result = self.memoization(nums, n-1)
+    #     return result
 
     # def recursion(self, nums, index):
     #     if index < 0:
