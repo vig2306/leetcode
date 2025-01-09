84f337c26cddd2af2dc98cadefd2348019545501
Vignesh <vignesh.sunder@gmail.com>
2025-01-08 18:10:06 -0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (13.63%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,30 +1,64 @@
 class Solution:
-    def tabulate(self, nums, dp):
-        dp[0] = nums[0]
+    #SPACE Optimised
+    def tabulate(self, nums, isFirstTaken):
         n = len(nums)
-        for i in range(1, n):
-            take = nums[i]
-            if i > 1:
-                take += dp[i-2]
-            
-            not_take = dp[i-1]
+        if isFirstTaken:
+            two_house = 0
+            one_house = nums[0]
+            first_index = 1
+            last_index = n - 2
+        else:
+            two_house = 0
+            one_house = nums[1]
+            first_index = 2
+            last_index = n - 1
 
-            dp[i] = max(take, not_take)
+        for i in range(first_index, last_index+1):
+            take = nums[i] + two_house
+            not_take = one_house
+
+            curr = max(take, not_take)
+            two_house = one_house
+            one_house = curr
         
-        return dp[n-1]
+        return one_house
 
 
     def rob(self, nums: List[int]) -> int:
         if len(nums) == 1:
             return nums[0]
-        n = len(nums)
-        dp1 = [-1]*(n-1)
-        dp2 = [-1]*(n-1)
 
-        res1 = self.tabulate(nums[:n-1], dp1)
-        res2 = self.tabulate(nums[1:], dp2)
+        res1 = self.tabulate(nums, True)
+        res2 = self.tabulate(nums, False)
 
         return max(res1, res2)
+
+    # def tabulate(self, nums, dp):
+    #     dp[0] = nums[0]
+    #     n = len(nums)
+    #     for i in range(1, n):
+    #         take = nums[i]
+    #         if i > 1:
+    #             take += dp[i-2]
+            
+    #         not_take = dp[i-1]
+
+    #         dp[i] = max(take, not_take)
+        
+    #     return dp[n-1]
+
+
+    # def rob(self, nums: List[int]) -> int:
+    #     if len(nums) == 1:
+    #         return nums[0]
+    #     n = len(nums)
+    #     dp1 = [-1]*(n-1)
+    #     dp2 = [-1]*(n-1)
+
+    #     res1 = self.tabulate(nums[:n-1], dp1)
+    #     res2 = self.tabulate(nums[1:], dp2)
+
+    #     return max(res1, res2)
 
     # def recursion(self, nums, index):
     #     if index < 0:
