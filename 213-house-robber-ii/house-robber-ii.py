7dc5b09baa91d8c753e5f1e483e141c6a36627cb
Vignesh <vignesh.sunder@gmail.com>
2025-01-08 18:02:02 -0800

    Time: 1 ms (17.11%) | Memory: 17.7 MB (21.58%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,32 +1,59 @@
 class Solution:
-    def recursion(self, nums, index):
-        if index < 0:
-            return 0
+    def tabulate(self, nums, dp):
+        dp[0] = nums[0]
+        n = len(nums)
+        for i in range(1, n):
+            take = nums[i]
+            if i > 1:
+                take += dp[i-2]
+            
+            not_take = dp[i-1]
+
+            dp[i] = max(take, not_take)
         
-        if index == 0:
-            self.memo[index] = nums[index]
-            return nums[index]
-        
-        if self.memo[index] != -1:
-            return self.memo[index]
+        return dp[n-1]
 
-        take = nums[index] + self.recursion(nums, index-2)
-        not_take= self.recursion(nums, index-1)
-
-        self.memo[index] = max(take, not_take)
-
-        return self.memo[index]
 
     def rob(self, nums: List[int]) -> int:
         if len(nums) == 1:
             return nums[0]
         n = len(nums)
-        self.memo = [-1]*(n-1)
-        res1 = self.recursion(nums[:n-1], n-2)
-        self.memo = [-1]*(n-1)
-        res2 = self.recursion(nums[1:], n-2)
+        dp1 = [-1]*(n-1)
+        dp2 = [-1]*(n-1)
+
+        res1 = self.tabulate(nums[:n-1], dp1)
+        res2 = self.tabulate(nums[1:], dp2)
 
         return max(res1, res2)
+
+    # def recursion(self, nums, index):
+    #     if index < 0:
+    #         return 0
+        
+    #     if index == 0:
+    #         self.memo[index] = nums[index]
+    #         return nums[index]
+        
+    #     if self.memo[index] != -1:
+    #         return self.memo[index]
+
+    #     take = nums[index] + self.recursion(nums, index-2)
+    #     not_take= self.recursion(nums, index-1)
+
+    #     self.memo[index] = max(take, not_take)
+
+    #     return self.memo[index]
+
+    # def rob(self, nums: List[int]) -> int:
+    #     if len(nums) == 1:
+    #         return nums[0]
+    #     n = len(nums)
+    #     self.memo = [-1]*(n-1)
+    #     res1 = self.recursion(nums[:n-1], n-2)
+    #     self.memo = [-1]*(n-1)
+    #     res2 = self.recursion(nums[1:], n-2)
+
+    #     return max(res1, res2)
 
     # def recursion(self, nums, index):
     #     if index < 0:
