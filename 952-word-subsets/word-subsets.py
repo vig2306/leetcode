5d2134c49a6b4fcb0ee7c834e6760180e467d1e5
Vignesh <vignesh.sunder@gmail.com>
2025-01-10 14:51:59 -0800

    Time: 494 ms (38.43%) | Memory: 20.9 MB (47.06%) - LeetSync

---
+++ 952-word-subsets/word-subsets.py
@@ -0,0 +1,19 @@
+class Solution:
+    def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
+        def count(word):
+            ans = [0] * 26
+            for letter in word:
+                ans[ord(letter) - ord('a')] += 1
+            return ans
+
+        bmax = [0] * 26
+        for b in words2:
+            for i, c in enumerate(count(b)):
+                bmax[i] = max(bmax[i], c)
+
+        ans = []
+        for a in words1:
+            if all(x >= y for x, y in zip(count(a), bmax)):
+                ans.append(a)
+        return ans
+        
