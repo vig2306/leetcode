bc983610dccd8560d59a424d7fdf70452528f774
Vignesh <vignesh.sunder@gmail.com>
2025-01-12 18:06:57 -0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (30.38%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,48 +1,42 @@
 class Solution:
+
+    #Memo
+    def recursion(self, i, j):
+        if i == 0 and j == 0:
+            return 1
+        if i < 0 or j < 0:
+            return 0
+        
+        if self.memo[i][j] != -1:
+            return self.memo[i][j]
+        
+        up = self.recursion(i-1, j)
+        left = self.recursion(i, j-1)
+
+        self.memo[i][j] = up + left
+        
+        return self.memo[i][j]
+
     def uniquePaths(self, m: int, n: int) -> int:
-        #Optimal - Combinations
-        #TC: O(M-1)
-        #SC: O(1)
-        N = m+n-2
-        r = m-1
-        res = 1
-        for i in range(1,r+1):
-            res = res*(N-r+i)/i
-            print(res)
+        self.memo = [[-1 for _ in range(n)] for _ in range(m)]
+        print(self.memo)
+        result = self.recursion(m-1, n-1)
+        return result
 
-        return int(res)
+
+
+    #Recursion
+    # def recursion(self, i, j):
+    #     if i == 0 and j == 0:
+    #         return 1
+    #     if i < 0 or j < 0:
+    #         return 0
         
-
-
-
-
-
-
-
-        #DP - Not the optimal
-
-        #TC:O(M*N)
-        #SC: O(M*N)
-
-    
-    #     grid = []
-    #     for i in range(m):
-    #         curr = []
-    #         for j in range(n):
-    #             if j == 0 or i ==0:
-    #                 curr.append(1)
-    #                 continue
-
-    #             curr.append(0)
-            
-    #         grid.append(curr)
+    #     up = self.recursion(i-1, j)
+    #     left = self.recursion(i, j-1)
         
-    #     for i in range(1, m):
-    #         for j in range(1,n):
-    #             grid[i][j] = grid[i-1][j] + grid[i][j-1]
-        
-    #     return grid[m-1][n-1]
-    
-
-
+    #     return up + left
+    # def uniquePaths(self, m: int, n: int) -> int:
+    #     result = self.recursion(m-1, n-1)
+    #     return result
         
