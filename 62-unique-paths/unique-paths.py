15fb826b079eefa7d37d045097ca17ce74a3fbd8
Vignesh <vignesh.sunder@gmail.com>
2025-01-12 18:09:58 -0800

    Time: 0 ms (100.00%) | Memory: 18 MB (11.65%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,27 +1,44 @@
 class Solution:
 
-    #Memo
-    def recursion(self, i, j):
-        if i == 0 and j == 0:
-            return 1
-        if i < 0 or j < 0:
-            return 0
-        
-        if self.memo[i][j] != -1:
-            return self.memo[i][j]
-        
-        up = self.recursion(i-1, j)
-        left = self.recursion(i, j-1)
-
-        self.memo[i][j] = up + left
-        
-        return self.memo[i][j]
-
+    #Tabulation - DP
     def uniquePaths(self, m: int, n: int) -> int:
-        self.memo = [[-1 for _ in range(n)] for _ in range(m)]
-        print(self.memo)
-        result = self.recursion(m-1, n-1)
-        return result
+        dp = [[-1 for _ in range(n)] for _ in range(m)]
+        for i in range(m):
+            dp[i][0] = 1
+        for j in range(n):
+            dp[0][j] = 1
+        
+        for i in range(1, m):
+            for j in range(1, n):
+                up = dp[i-1][j]
+                down = dp[i][j-1]
+
+                dp[i][j] = up + down
+
+        return dp[m-1][n-1]
+
+    #Memo
+    # def recursion(self, i, j):
+    #     if i == 0 and j == 0:
+    #         return 1
+    #     if i < 0 or j < 0:
+    #         return 0
+        
+    #     if self.memo[i][j] != -1:
+    #         return self.memo[i][j]
+        
+    #     up = self.recursion(i-1, j)
+    #     left = self.recursion(i, j-1)
+
+    #     self.memo[i][j] = up + left
+        
+    #     return self.memo[i][j]
+
+    # def uniquePaths(self, m: int, n: int) -> int:
+    #     self.memo = [[-1 for _ in range(n)] for _ in range(m)]
+    #     print(self.memo)
+    #     result = self.recursion(m-1, n-1)
+    #     return result
 
 
 
