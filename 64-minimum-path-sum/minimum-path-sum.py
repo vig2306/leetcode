3fd9d28d897a3590e7cc58d6f0db89943bf0fc46
Vignesh <vignesh.sunder@gmail.com>
2025-01-14 19:03:30 -0800

    Time: 23 ms (30.97%) | Memory: 20.1 MB (41.41%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -1,27 +1,48 @@
 class Solution:
-    #Memo
-    def recursion(self, grid, i, j):
-        if i < 0 or j < 0:
-            return float('inf')
-        if i==0 and j==0:
-            return grid[0][0]
-        
-        if self.memo[i][j] != -1:
-            return self.memo[i][j]
-        
-        up = grid[i][j] + self.recursion(grid, i-1, j)
-        left = grid[i][j] + self.recursion(grid, i, j-1)
-
-        self.memo[i][j] = min(up,left)
-
-        return self.memo[i][j]
-        
+    #Tabulation
     def minPathSum(self, grid: List[List[int]]) -> int:
         m = len(grid)
         n = len(grid[0])
-        self.memo = [[-1 for _ in range(n)] for _ in range(m)]
-        res = self.recursion(grid, m-1, n-1)
-        return res
+        dp = [[-1 for _ in range(n)] for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                if i == 0 and j == 0:
+                    dp[i][j] = grid[i][j]
+                    continue
+                up = float('inf')
+                left = float('inf')
+                if i > 0:
+                    up = grid[i][j] + dp[i-1][j]
+                if j > 0:
+                    left = grid[i][j] + dp[i][j-1]
+                
+                dp[i][j] = min(up, left)
+
+        return dp[m-1][n-1]
+
+    #Memo
+    # def recursion(self, grid, i, j):
+    #     if i < 0 or j < 0:
+    #         return float('inf')
+    #     if i==0 and j==0:
+    #         return grid[0][0]
+        
+    #     if self.memo[i][j] != -1:
+    #         return self.memo[i][j]
+        
+    #     up = grid[i][j] + self.recursion(grid, i-1, j)
+    #     left = grid[i][j] + self.recursion(grid, i, j-1)
+
+    #     self.memo[i][j] = min(up,left)
+
+    #     return self.memo[i][j]
+        
+    # def minPathSum(self, grid: List[List[int]]) -> int:
+    #     m = len(grid)
+    #     n = len(grid[0])
+    #     self.memo = [[-1 for _ in range(n)] for _ in range(m)]
+    #     res = self.recursion(grid, m-1, n-1)
+    #     return res
     
     # #Recursion
     # def recursion(self, grid, i, j):
