ef866873fb7dc311d400aeb919b4da7406e42c48
Vignesh <vignesh.sunder@gmail.com>
2025-01-19 16:58:15 -0800

    Time: 0 ms (100.00%) | Memory: 18.1 MB (13.78%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.py
+++ 225-implement-stack-using-queues/implement-stack-using-queues.py
@@ -1,40 +1,36 @@
 class MyStack:
 
     def __init__(self):
-        self.q1 = []
-        self.q2 = []
+        self.queue = deque()
         
-
     def push(self, x: int) -> None:
-        if len(self.q1) == 0:
-            self.q1.append(x)
-        else:
-            num = self.q1.pop()
-            self.q2.append(num)
-            self.q1.append(x)
-        
-    def pop(self) -> int:
-        # if len(self.q1) == 0:
-        #     return -1
+        self.queue.append(x)
+        length = len(self.queue)
+        while length > 1:
+            val = self.queue.popleft()
+            self.queue.append(val)
+            length -= 1
 
-        top_num = self.q1.pop()
-        while len(self.q2) > 1:
-            curr_top = self.q2.pop(0)
-            self.q1.append(curr_top)
-        self.q1, self.q2 = self.q2, self.q1
-        return top_num
+        
+
+    def pop(self) -> int:
+        val = -1
+        if self.queue:
+            val = self.queue.popleft()
+        return val
 
     def top(self) -> int:
-        # if len(self.q1) == 0:
-        #     return -1
-        top_num = self.q1[0]
-        return top_num
+        val = -1
+        if self.queue:
+            val = self.queue[0]
         
-    def empty(self) -> bool:
-        if not self.q1 and not self.q2:
-            return True
-        return False
+        return val
 
+    def empty(self) -> bool:
+        if self.queue:
+            return False
+        
+        return True
         
 
 
