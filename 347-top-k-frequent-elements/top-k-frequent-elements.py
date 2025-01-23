bbe8c25f6ac40dd52d20d616223e20ad4fbef109
Vignesh <vignesh.sunder@gmail.com>
2025-01-22 22:29:24 -0800

    Time: 3 ms (92.12%) | Memory: 21.1 MB (64.43%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,5 +1,32 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+
+        '''Better: Keep minHeap of size K and then find the topK in Dictionary 
+        instead of sorting
+        TC -> O(NLogk)
+        SC -> O(N)'''
+        countDict = {}
+        for i in range(len(nums)):
+            countDict[nums[i]] = countDict.get(nums[i], 0) + 1
+        
+        minHeap = []
+        for key, value in countDict.items():
+            if len(minHeap) < k:
+                heapq.heappush(minHeap, (value, key))
+            
+            else:
+                if value >= minHeap[0][0]:
+                    heapq.heappop(minHeap)
+                    heapq.heappush(minHeap, (value, key))
+        res = []
+        while minHeap:
+            key, value = heapq.heappop(minHeap)
+            res.append(value)
+        return res
+
+                
+            
+
         '''Brute Force: Go through array, store in hashmap, sort by value
             find the top k frequent elements
             TC-> O(NLogn)
