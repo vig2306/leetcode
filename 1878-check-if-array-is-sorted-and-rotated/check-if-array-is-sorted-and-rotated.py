a164d3b6d68a1e4f39fad55cc7cabf8953a10960
Vignesh <vignesh.sunder@gmail.com>
2025-02-01 17:11:37 -0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (13.49%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
@@ -2,9 +2,9 @@ class Solution:
     def check(self, nums: List[int]) -> bool:
         n = len(nums)
         startIndex = -1
-        array = []
+        # array = []
         for i in range(0, n):
-            array.append(nums[i])
+            # array.append(nums[i])
             prev = nums[i-1]
             nextnumber = nums[(i+1)%n]
             
@@ -15,13 +15,15 @@ class Solution:
         if startIndex == -1:
             return True
         
-        for i in range(n):
-            array.append(nums[i])
+        # for i in range(n):
+        #     array.append(nums[i])
         
-        print(array)
+        # print(array)
         
-        for i in range(startIndex, n+startIndex-1):
-            if array[i] > array[i+1]:
+        for i in range(n-1):
+            curr_index = (i + startIndex)%n
+            next_index = (i + startIndex + 1)%n
+            if nums[curr_index] > nums[next_index]:
                 return False
 
         return True
